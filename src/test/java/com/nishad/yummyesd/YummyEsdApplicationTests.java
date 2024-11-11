12cf602aca3f047618b8a1194abbb2539c187880
Nishad Bagade <NishadUmesh.Bagade@iiitb.ac.in>
2024-11-12 00:00:17 +0530

    Initial commit

---
+++ src/test/java/com/nishad/yummyesd/YummyEsdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nishad.yummyesd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyEsdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
